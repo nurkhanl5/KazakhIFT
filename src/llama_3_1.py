c433f223992d60a038dad776ba98f34829f14017
Nurkhan Laiyk <86560310+nurkhanl5@users.noreply.github.com>
2024-12-05 20:39:06 +0400

    Update llama_3_1.py

--- src/llama_3_1.py
+++ src/llama_3_1.py
@@ -1,11 +1,4 @@
 from huggingface_hub import login
-#from kaggle_secrets import UserSecretsClient
-#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
-'''secret_label = "HF Hub"
-secret_value = UserSecretsClient().get_secret(secret_label)
-login(token=secret_value)
-'''
-# Replace "your_huggingface_token" with your actual Hugging Face token
 huggingface_token = "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"
 
 # Log in to Hugging Face Hub
